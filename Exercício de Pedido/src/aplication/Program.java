223b60d998dfa419130b9fb119b7fe7bf6f59385
Luiz Neto <luiz.remor@icloud.com>
2021-09-29 13:52:02 -0300

    Atualização

---
+++ Exercício de Pedido/src/aplication/Program.java
@@ -0,0 +1,23 @@
+package aplication;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import entities.Products;
+import entities.HourContract;
+import entities.Client;
+import entities.enums.Status;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+
+		Scanner sc = new Scanner(System.in);
+		
+		sc.close();
+
+	}
+
+}
