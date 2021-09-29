f565a93d94145a28bd3a9e5a640f06766baf1ae8
Luiz Neto <luiz.remor@icloud.com>
2021-09-29 16:26:42 -0300

    Atualização

--- Programa Post/src/application/Program.java
+++ Programa Post/src/application/Program.java
@@ -14,8 +14,7 @@ public class Program {
 
 		Comment c1 = new Comment("Have a nice trip!");
 		Comment c2 = new Comment("Wow that's awesome!");
-		Post p1 = new Post(sdf.parse("21/06/2018 13:05:44"), "Traveling to New Zealand",
-				"I'm going to visit this wonderful country!", 12);
+		Post p1 = new Post(sdf.parse("21/06/2018 13:05:44"), "Traveling to New Zealand", "I'm going to visit this wonderful country!", 12);
 		p1.addComment(c1);
 		p1.addComment(c2);
 
