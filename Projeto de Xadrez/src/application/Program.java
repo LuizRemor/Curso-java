c6c8eba155e961ba30a244a85eb804bc6f53c86c
Luiz Neto <luiz.remor@icloud.com>
2021-10-05 17:42:42 -0300

    Primeiro commit do programa de xadrez

--- Projeto de Xadrez/src/application/Program.java
+++ Projeto de Xadrez/src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-
+		ChessMatch chesMatch = new ChessMatch();
+		
+		UI.printBoard(chesMatch.getPieces());
+		
 	}
 
 }
